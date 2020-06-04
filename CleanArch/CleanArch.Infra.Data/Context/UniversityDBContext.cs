7044e1eed3f5c33b0e19dda45b4c9a085b7d7b2e
Hooshmand <Hooshmand@HOOSHMAND-PC>
2020-06-04 13:38:13 -0700

    create data

---
+++ CleanArch/CleanArch.Infra.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArch.Domain.Models;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
