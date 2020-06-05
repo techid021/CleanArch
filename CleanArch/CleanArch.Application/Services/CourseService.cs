d2bf512b6d7f959456a56f3710e1ddf421981167
Hooshmand <Hooshmand@HOOSHMAND-PC>
2020-06-05 12:17:40 -0700

    hvgnv

---
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private ICourseRepository courseRepository;
+        public CourseService(ICourseRepository courseRepository)
+        {
+            this.courseRepository = courseRepository;
+        }
+        public CourseViewModel GetCourse()
+        {
+            return new CourseViewModel { Courses = courseRepository.GetCourses() };
+        }
+    }
+}
---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Application.Services;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UniversityDBContext universityDBContext;
+        public CourseRepository(UniversityDBContext universityDBContext)
+        {
+            this.universityDBContext = universityDBContext;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return universityDBContext.Courses;
+        }
+    }
+}
