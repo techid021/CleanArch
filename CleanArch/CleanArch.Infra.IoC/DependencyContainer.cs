e48d3a18a9434328c40b26ead5c3cf432d7a5637
Hooshmand <Hooshmand@HOOSHMAND-PC>
2020-06-05 12:58:56 -0700

    ssadas

---
+++ CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Application.Services;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection service)
+        {
+            //Application Layer
+            service.AddScoped<ICourseService, CourseService>();
+
+            //Infra Data Layer
+            service.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
