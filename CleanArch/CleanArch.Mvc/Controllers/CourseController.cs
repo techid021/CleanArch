f0f6589d9d94e3919460b40c897f788fbe29b9b2
Hooshmand <Hooshmand@HOOSHMAND-PC>
2020-06-05 13:48:10 -0700

    fggffh

---
+++ CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Services;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService courseService;
+        public CourseController(ICourseService courseService)
+        {
+            this.courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            CourseViewModel model = courseService.GetCourse();
+            return View(model);
+        }
+
+    }
+}
